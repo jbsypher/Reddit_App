b7eb522821b36e0412ef98009d65c0d5a0994e01
Jonathan Barta <jonathanbarta3@gmail.com>
2023-11-27 21:50:34 -0500

    Initial Commit Reddit App

---
+++ Reddit_App/Reddit_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reddit_App
+//
+//  Created by Jonathan Barta on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reddit_App/Reddit_App/Reddit_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reddit_AppApp.swift
+//  Reddit_App
+//
+//  Created by Jonathan Barta on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Reddit_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
